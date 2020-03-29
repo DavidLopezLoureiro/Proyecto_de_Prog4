3a2247215553c57266070d1d01eb71714fcae8e2
David <33113652+DavidLopezLoureiro@users.noreply.github.com>
2020-03-29 12:05:28 +0200

    Main.c cambiado

--- Main.c
+++ Main.c
@@ -1,9 +1,72 @@
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "Aula.h"
+#include "Alumno.h"
+#include "Profesor.h"
+#include "Asignatura.h"
 
 
-int main(void){
+#define MAX_LINE 10
 
 
-return 0;
+
+void clearIfNeeded(char *str, int max_line)
+	{
+		// Limpia los caracteres de m·s introducidos
+		if ((strlen(str) == max_line-1) && (str[max_line-2] != '\n'))
+			while (getchar() != '\n');
+	}
+
+	char mostrarMenu()
+	{
+		printf("1. Introducir asignatura en aula \n");
+		printf("2. \n");
+		printf("Pulsar 'q' para salir\n");
+		printf("\n");
+		printf("Opcion: ");
+		fflush(stdout);
+		char linea[MAX_LINE];
+		fgets(linea, MAX_LINE, stdin);
+		clearIfNeeded(linea, MAX_LINE);
+		return * linea;
 }
+
+
+
+int main() {
+
+	printf("1. Introducir asignatura en aula \n");
+
+
+	Profesor felipe;
+
+	felipe.nombre = "Felipe";
+	felipe.DNI = "234212";
+
+
+
+	Asignatura mate;
+
+	mate.Codigo = 00;
+	mate.carrera = "general";
+	mate.creditos = 6;
+	mate.curso = 1;
+	mate.nombre = "mate";
+	mate.profe = felipe;
+
+
+
+	Aula aula1;
+
+	aula1.codigo = "01";
+	aula1.ocupadapor[0][0] = mate;
+
+	return 0;
+
+
+
+	}
+
